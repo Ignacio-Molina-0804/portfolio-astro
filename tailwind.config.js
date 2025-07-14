e9332baba225e9bcd756915d7f583e993251a2a5
Ignacio Molina <molinagranda@gmail.com>
2025-07-14 19:25:39 -0300

    Tailwind Funcionando

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+    "./public/**/*.html"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // require('@tailwindcss/typography')
+  ],
 };
