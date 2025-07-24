1dce429b16d627048137a6cf137a0cf3f88a9936
Ignacio Molina <molinagranda@gmail.com>
2025-07-24 14:38:00 -0300

    Cambio en el Boton de Idioma

--- src/scripts/ChangeLenguage.ts
+++ src/scripts/ChangeLenguage.ts
@@ -1,12 +1,26 @@
-const languageBtn = document.getElementById("language-toggle") as HTMLButtonElement | null;
-const languageLabel = document.getElementById("language-label") as HTMLSpanElement | null;
+const languageBtn = document.getElementById(
+  "language-toggle"
+) as HTMLButtonElement | null;
+const languageLabel = document.getElementById(
+  "language-label"
+) as HTMLSpanElement | null;
 
 if (languageBtn && languageLabel) {
   const currentPath: string = window.location.pathname;
   const isEnglish: boolean = currentPath.startsWith("/en");
 
   // Establecer el texto del botón según el idioma actual
-  languageLabel.textContent = isEnglish ? "ES" : "EN";
+  languageLabel.innerHTML = isEnglish
+    ? `<img
+        src="https://flagcdn.com/w2560/us.webp"
+        alt="Bandera de Estados Unidos"
+        class="block mx-auto w-[23px] h-[20px] rounded-full select-none cursor-zoom-in bg-[hsl(0,0%,90%)] transition-colors duration-300"
+      />`
+    : `<img
+        src="https://flagcdn.com/w2560/ar.webp"
+        alt="Bandera de Argentina"
+        class="block mx-auto w-[23px] h-[20px] rounded-full select-none cursor-zoom-in bg-[hsl(0,0%,90%)] transition-colors duration-300"
+      />`;
 
   languageBtn.addEventListener("click", () => {
     const newPath: string = isEnglish
